7f5c660e07b098410d578556e5582e9d31813d2b
Rodrigo Nohlack <rodrigo.nohlack@gmail.com>
2026-05-22 15:08:42 -0300

    feat(embargos): stores barrel

---
+++ src/apps/embargos/stores/index.ts
@@ -0,0 +1,4 @@
+export { useAIStore, persistApiKeys, selectProvider, selectCurrentModel, selectCurrentApiKey } from './useAIStore';
+export { useDocumentStore } from './useDocumentStore';
+export { useSynthesisStore } from './useSynthesisStore';
+export { useDraftStore, emptySection } from './useDraftStore';
